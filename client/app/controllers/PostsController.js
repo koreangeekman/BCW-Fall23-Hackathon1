88aa998ea88f14a57f3558ac88cd766f104a0fd5
DJ Aaron <koreangeekman@gmail.com>
2023-10-20 12:08:12 -0600

    drew posts to page

--- client/app/controllers/PostsController.js
+++ client/app/controllers/PostsController.js
@@ -1,11 +1,22 @@
 import { AppState } from "../AppState.js"
 import { postsService } from "../services/PostsService.js"
 import { Pop } from "../utils/Pop.js"
+import { setHTML } from "../utils/Writer.js"
+
+
+
+function _drawPosts() {
+  const posts = AppState.posts
+  let content = ''
+  posts.forEach(post => content += post.PostCardTemplate)
+  setHTML('imageBoard', content)
+}
 
 export class PostsController {
   constructor() {
     // console.log('controller is good')
     AppState.on('account', this.getPosts)
+    AppState.on('posts', _drawPosts)
   }
 
 
--- client/app/services/PostsService.js
+++ client/app/services/PostsService.js
@@ -1,9 +1,13 @@
+import { AppState } from "../AppState.js"
+import { Post } from "../models/Post.js"
 import { api } from "./AxiosService.js"
 
 class PostsService {
   async getPosts() {
     const res = await api.get('api/posts')
-    console.log('Got Posts', res.data);
+    // console.log('Got Posts', res.data);
+    const newPosts = res.data.map((postPOJO) => new Post(postPOJO))
+    AppState.posts = newPosts
   }
 
 }
