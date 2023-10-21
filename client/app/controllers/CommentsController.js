5fd71bb44e2446c2cd2d980a88ba6b287ca5959a
Rhys Vickers <rs.vickers5@gmail.com>
2023-10-20 18:02:51 -0600

    create comment

--- client/app/controllers/CommentsController.js
+++ client/app/controllers/CommentsController.js
@@ -1,7 +1,55 @@
+import { AppState } from "../AppState.js";
+import { commentsService } from "../services/CommentsService.js";
+import { getFormData } from "../utils/FormHandler.js";
+import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js";
+
+
+
+
+
+function _drawComments() {
+    const comments = AppState.comments
+    let content = ''
+    comments.forEach(comment => content += comment.CommentTemplate)
+    setHTML('postDetails', content)
+}
 
 
 export class CommentsController {
     constructor() {
-        console.log('we good');
+        AppState.on('comments', _drawComments)
+        // AppState.on('activePost', this.getCommentsByPostId)
+    }
+
+    async createComment(event) {
+        try {
+            event.preventDefault()
+            const form = event.target
+            const commentData = getFormData(form)
+            // console.log('comment data', commentData);
+            await commentsService.createComment(commentData)
+            form.reset()
+        } catch (error) {
+            console.error(error);
+            Pop.error(error)
+        }
+    }
+
+    async getComments() {
+        try {
+            await commentsService.getComments()
+        } catch (error) {
+            Pop.error(error)
+        }
+    }
+
+
+    async getCommentsByPostId() {
+        try {
+            await commentsService.getCommentsByPostId()
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
--- client/app/services/CommentsService.js
+++ client/app/services/CommentsService.js
@@ -1,4 +1,37 @@
+import { AppState } from "../AppState.js";
+import { Comment } from "../models/Comment.js";
+import { api } from "./AxiosService.js"
+
 class CommentsService {
+
+    async getCommentsByPostId() {
+        const post = AppState.activePost
+        const res = await api.get(`api/posts/${post.id}/comments`)
+        console.log('created comment', res.data)
+        const newComments = res.data.map(pojo => new Comment(pojo))
+
+        AppState.comments = newComments
+    }
+
+
+
+    async createComment(commentData) {
+        const postId = AppState.activePost.id
+        commentData.postId = postId;
+        const res = await api.post('api/comments', commentData)
+        console.log('created comment', res.data);
+        const newComment = new Comment(res.data)
+        AppState.comments.push(newComment)
+        AppState.emit('comments')
+    }
+
+
+    async getComments() {
+        const res = await api.get('api/comments')
+        console.log('got comments')
+        const newComments = res.data.map(commentPOJO => new Comment(commentPOJO))
+        AppState.comments = newComments
+    }
 
 }
 
