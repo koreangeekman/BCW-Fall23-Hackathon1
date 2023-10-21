4cc660980c005af431aaf7f3d582051591aec8cc
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-10-20 21:58:20 -0600

    create post fixed

--- client/app/controllers/AuthController.js
+++ client/app/controllers/AuthController.js
@@ -10,9 +10,7 @@ function drawUser() {
   const template = /* html */ `
     <div class="d-block">
       <div class="d-flex mb-2">${userAvatar}${button}</div>
-      <button class="btn btn-outline-dark d-flex justify-content-end" data-bs-toggle="modal"
-        data-bs-target="#postFormModal">Create Post
-      </button>
+      
     </div>
   `
   // @ts-ignore
@@ -44,11 +42,20 @@ export class AuthController {
 }
 
 function authButton(user) {
+
   if (AuthService.loading) { return '' }
   return user && user.isAuthenticated
     ? /* html */ `
+    <div class="d-inline">
+    <img class="rounded-circle" src="${user.picture}" alt="${user.nickname}" height="45"/>
+    <span class="mx-1">${user.nickname}</span>
         <button class="btn btn-small btn-white text-muted selectable" onclick="app.AuthController.logout()">
         <i class="mdi mdi-logout-variant f-16 text-white"></i></button>
+    
+        <button class="btn btn-outline-dark d-flex form-control justify-content-center mt-2 " data-bs-toggle="modal"
+        data-bs-target="#postFormModal">Create Post
+      </button>
+    </div>
   `
     : /* html */ `
     <button class="btn btn-dark selectable" onclick="app.AuthController.login()">login</button>
@@ -59,8 +66,7 @@ function avatarTemplate(account) {
   return account
     ? /* html */ `
     <div class="mr-2">
-      <img class="rounded-circle" src="${account.picture}" alt="${account.name}" height="45"/>
-      <span class="mx-1">${account.name}</span>
+
       </div>`
     : AuthService.loading
       ? /* html */ `
